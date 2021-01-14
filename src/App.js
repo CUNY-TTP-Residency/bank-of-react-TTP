4fc6870acf47971ed89fe4fe32aa9218f7c264c9
Gezim Hakramaj <gezimhakramaj@gmail.com>
2021-01-13 22:38:36 -0500

    Initial project
    Filled in all the source code from the google doc. Now only to add the functionality requested from the assignment.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/LogIn';
+    
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99'
+      }
+    }
+  }
 
-export default App;
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
+  render() {
+    const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} accountBalance={this.state.accountBalance}/>)
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>)
+    
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" render={LogInComponent}/>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+        </div>
+      </Router>
+    );
+  }
+}   
